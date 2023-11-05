2339603366ad2bf97f32e7bf1bc1e93cdfa75320
riyaz <riyaz3582@gmail.com>
2023-11-05 11:09:59 +0600

    created assignments get and post api

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'http://localhost:5174',
-        'https://car-doctor-crud-38f97.web.app',
-         'https://car-doctor-crud-38f97.firebaseapp.com'
+        'http://localhost:5174'
         ],
     credentials:true
 }));
@@ -21,9 +19,54 @@ app.use(express.json());
 app.use(cookieParser())
 
 
+// MONGODB
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gx7mkcg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const dbConnect = async () => {
+    try {
+        await client.connect();
+        console.log("Database Connected successfully ✅");
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+  }
+  dbConnect();
+
+// MONGO COLLECTIONS
+
+const assignmentsCollection = client.db("assignmentsDB").collection("assignments");
+
+// GET POST PATCH DELETE
+
 app.get('/', (req, res) => {
     res.send('Assignment time running out')
 })
+
+// ASSIGNMENTS
+app.get('/assignments', async(req, res)=>{
+    const assignments = await assignmentsCollection.find().toArray()
+    res.send(assignments)
+})
+
+app.post('/assignments', async(req, res)=>{
+    const assignment = req.body
+    const result = await assignmentsCollection.insertOne(assignment)
+    console.log(result)
+    res.send(result)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Assignment server running on ${port}`)
